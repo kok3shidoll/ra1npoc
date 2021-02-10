e4d471180ad709c6f1efd2887f65c7646e98416f
Dora2 <42214504+dora2-iOS@users.noreply.github.com>
2021-02-11 04:21:14 +0900

    fixed mk_iphoneos_arm64.sh

--- src/mk_iphoneos_arm64.sh
+++ src/mk_iphoneos_arm64.sh
@@ -2,4 +2,4 @@
 
 xcrun -sdk iphoneos clang main.c iousb.c checkra1n_common.c checkra1n_t8010_t8015.c checkra1n_s8000.c -o ra1npoc_iphoneos_arm64 -I./include -framework IOKit -framework CoreFoundation -Os -DHAVE_DEBUG -DIPHONEOS_ARM -DIPHONEOS_LOWSPEC -arch arm64
 
-codesign -f -s - ra1npoc_arm64
+codesign -f -s - ra1npoc_iphoneos_arm64
