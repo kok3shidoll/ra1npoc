ab03cd9ec2115bccbb9d89661cbd46e9e37d1c8b
Dora2 <42214504+dora2-iOS@users.noreply.github.com>
2021-08-27 18:38:37 +0900

    Update checkra1n_t8010_t8015.c

--- src/checkra1n_t8010_t8015.c
+++ src/checkra1n_t8010_t8015.c
@@ -212,6 +212,8 @@ int checkra1n_t8010_t8015(io_client_t client, uint16_t cpid, checkra1n_payload_t
     
     LOG_PROGRESS("[%s] reconnecting", __FUNCTION__);
     result = io_reenumerate(client);
+    DEBUGLOG("[%s] USBDeviceReEnumerate: %x", __FUNCTION__, result);
+    
     io_close(client);
     client = NULL;
     usleep(10000);
